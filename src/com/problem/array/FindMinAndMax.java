6043157e44cc64c22620cfeac3bba0560b7ba8ab
e5657322 <e5657322@BLR1-LTHVH12F3.FNFIS.com>
2022-12-14 20:34:49 +0530

    Find min and max from the array

---
+++ src/com/problem/array/FindMinAndMax.java
@@ -0,0 +1,43 @@
+package com.problem.array;
+
+// Basic
+public class FindMinAndMax {
+    
+    static class Pair  
+    {  
+        long first, second;  
+        
+        public Pair(long first, long second)  
+        {  
+            // Min
+            this.first = first; 
+            
+            // Max
+            this.second = second;  
+        }  
+    }
+
+    static Pair getMinMax(long a[], long n) {
+        
+        Pair result = new Pair(a[0], a[0]);
+
+        for (int i = 1; i < n; i++) {
+
+            if (a[i] < result.first)
+                result.first = a[i];
+            else if (a[i] > result.second)
+                result.second = a[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        long[] arr = { 1, 5, 100, 5673, 4 };
+
+        Pair pair = FindMinAndMax.getMinMax(arr, arr.length);
+
+        System.out.println(pair.first + " " + pair.second);
+    }
+}
