13590374f9de2f43d9a3d5e483f1cd4da084870d
e5657322 <e5657322@BLR1-LTHVH12F3.FNFIS.com>
2022-12-14 20:28:55 +0530

    Reverse the Array

---
+++ src/com/problem/array/ReverseTheArray.java
@@ -0,0 +1,32 @@
+package com.problem.array;
+
+// Easy
+public class ReverseTheArray {
+    
+    public static void reverse(int[] arr, int n) {
+        
+        int low = 0;
+        int high = n-1;
+        
+        while (low < high) {
+            
+            int temp = arr[low];
+            arr[low] = arr[high];
+            arr[high] = temp;
+            
+            low++;
+            high--;
+        }
+    }
+
+    public static void main(String[] args) {
+        
+        int[] arr = { 1, 2, 3, 4, 5, 6, 7 };
+        
+        ReverseTheArray.reverse(arr, arr.length);
+        
+        for (int element : arr) 
+            System.out.print(element + " ");
+    }
+
+}
