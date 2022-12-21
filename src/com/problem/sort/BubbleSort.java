b83f05794e7d712aa2babcd0ceaaf015ad272daf
e5657322 <e5657322@BLR1-LTHVH12F3.FNFIS.com>
2022-12-21 12:14:24 +0530

    Update

---
+++ src/com/problem/sort/BubbleSort.java
@@ -0,0 +1,39 @@
+package com.problem.sort;
+
+public class BubbleSort {
+    
+    static void sort(int[] arr) {
+        
+        for (int i=0; i<arr.length-1; i++) {
+            
+            boolean swapped = false;
+            
+            for (int j=0; j<arr.length-i-1; j++) {
+                
+                if (arr[j]>arr[j+1]) {
+                    
+                    swap(arr, j, j+1);
+                    swapped = true;
+                }
+            }
+            
+            if (!swapped) break;
+        }
+    }
+    
+    static void swap(int[] arr, int start, int end) {
+        
+        int temp = arr[start];
+        arr[start] = arr[end];
+        arr[end] = temp;
+    }
+
+    public static void main(String[] args) {
+        
+        int arr[] = { 6, 2, 5, 3, 7, 1, 9, 4};
+        sort(arr);
+        
+        for (int element : arr) System.out.print(element + " ");
+    }
+
+}
