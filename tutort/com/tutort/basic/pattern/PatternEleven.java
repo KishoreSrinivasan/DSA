0023fe5cf9678b6bf2b567dea786cf19cc99940a
User <User@WINDOWS-QQ50T7C>
2023-01-09 14:55:14 +0530

    Updated

---
+++ tutort/com/tutort/basic/pattern/PatternEleven.java
@@ -0,0 +1,31 @@
+package com.tutort.basic.pattern;
+
+public class PatternEleven {
+	
+	static void print(int n) {
+
+		int flag = 1;
+		for (int i = 0; i < n; i++) {
+
+			for (int j = 0; j < n; j++) {
+				
+				if ((i%2) == 0 && j==0) flag = 1;
+
+				if (j <= i) {
+					System.out.print(flag + " ");
+					flag = flag==1?0:1;
+				}
+				else
+					System.out.print("" + " ");
+			}
+			System.out.println();
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		print(10);
+	}
+
+}
