ce5e902213e2cf9154fd272e2935dd578864029f
User <User@WINDOWS-QQ50T7C>
2023-01-09 15:22:58 +0530

    Updated

---
+++ tutort/com/tutort/basic/pattern/PatternTwelve.java
@@ -0,0 +1,33 @@
+package com.tutort.basic.pattern;
+
+public class PatternTwelve {
+	
+	static void print(int n) {
+		
+		for (int i=0; i<n; i++) {
+			
+			int count = 1;
+			
+			for (int j=0; j<n; j++) {
+				
+				if (j<=i) System.out.print(count++ + " ");
+				else System.out.print(" " + " ");
+			}
+			
+			for (int j=n-1; j>=0; j--) {
+				
+				if (j<=i) System.out.print(--count + " ");
+				else System.out.print(" " + " ");
+			}
+			System.out.println();
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		print(100);
+	}
+
+}
